f4d117a32fe371ad84893590c0eee8d80f35f70f
Antanas Kalkauskas <a.kalkauskas@gmail.com>
2025-11-04 14:08:39 +0800

    Replace parray with pbytes data structure for Wasm memory representation in the extracted interpreter.

--- interpreter/verified_wrapper/memRepWrapper.ml
+++ interpreter/verified_wrapper/memRepWrapper.ml
@@ -1,14 +1,19 @@
-open Parray
+(* open Pbytes *)
 
-let memRepAt memRep n = Parray.get memRep n
+let memRepAt memRep n = Pbytes.get_char memRep n
 
-let memRepAppend memRep n byte = Parray.init (length memRep + n) (fun idx -> if idx < (length memRep) then (Parray.get memRep idx) else byte)
+let memRepAppend = Pbytes.append
+  (* Pbytes.append memRep n byte *)
+  (* let newMemRep = (Pbytes.make (length memRep + n) byte) in
+  Pbytes.blit memRep 0 newMemRep 0 (length memRep);
+  newMemRep *)
+  (* Pbytes.init (length memRep + n) (fun idx -> if idx < (length memRep) then (Pbytes.get_char memRep idx) else byte) *)
 
-let rec memRepReadBytesHelper memRep n m arr = if (0 < m) then memRepReadBytesHelper memRep n (m-1) ((Parray.get memRep (n+m-1))::arr) else arr
+let rec memRepReadBytesHelper memRep n m arr = if (0 < m) then memRepReadBytesHelper memRep n (m-1) ((Pbytes.get_char memRep (n+m-1))::arr) else arr
 
 let memRepReadBytes memRep n m = memRepReadBytesHelper memRep n m []
 
 let rec memRepWriteBytes memRep n arr =
   match arr with
     [] -> memRep
-  | (x :: xs) -> memRepWriteBytes (Parray.set memRep n x) (n+1) xs
+  | (x :: xs) -> memRepWriteBytes (Pbytes.set_char memRep n x) (n+1) xs
