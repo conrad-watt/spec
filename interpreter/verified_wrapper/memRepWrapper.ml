3266df9dd76b12ad1da548343a391d0ee3869d54
Antanas Kalkauskas <a.kalkauskas@gmail.com>
2025-11-04 14:16:05 +0800

    Clean up commentes in memRepWrapper.ml.

--- interpreter/verified_wrapper/memRepWrapper.ml
+++ interpreter/verified_wrapper/memRepWrapper.ml
@@ -1,13 +1,7 @@
-(* open Pbytes *)
 
 let memRepAt memRep n = Pbytes.get_char memRep n
 
 let memRepAppend = Pbytes.append
-  (* Pbytes.append memRep n byte *)
-  (* let newMemRep = (Pbytes.make (length memRep + n) byte) in
-  Pbytes.blit memRep 0 newMemRep 0 (length memRep);
-  newMemRep *)
-  (* Pbytes.init (length memRep + n) (fun idx -> if idx < (length memRep) then (Pbytes.get_char memRep idx) else byte) *)
 
 let rec memRepReadBytesHelper memRep n m arr = if (0 < m) then memRepReadBytesHelper memRep n (m-1) ((Pbytes.get_char memRep (n+m-1))::arr) else arr
 
